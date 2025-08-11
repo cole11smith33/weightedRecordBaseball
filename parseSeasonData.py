7c49998abf7a7624487722d25036994b26e41eaa
cole11smith33 <cole11smith33@gmail.com>
2025-08-11 16:42:16 -0400

    wrote function to split boxscore argument into 4 variables (away team, score, home team, score)

--- parseSeasonData.py
+++ parseSeasonData.py
@@ -1,12 +1,56 @@
 file_path = "data/resultsOfEveryRegSeasonGameIn2024.txt"
 
 #function to parse the line
-#e.g New York Mets (0) @ Atlanta Braves (3)  Boxscore becomes New York Mets (0) @ Atlanta Braves (3)     
+#e.g New York Mets (0) @ Atlanta Braves (3) Boxscore becomes New York Mets (0) @ Atlanta Braves (3)     
 def parseLine(parsedLine):
         parsedLine = line.strip()
+        if "2024" in parsedLine: # remove lines with the date games were played, to clean up the data more
+             parsedLine = ""
         parsedLine = parsedLine.replace("Boxscore", "")
         return parsedLine
 
+
+# temp code, will make more efficient by avoiding the loop and just using the built-in split function
+def parseIntoResults(parsedLine):
+        datapoint = ""
+        mode=0
+        teams = parsedLine.split("@")
+        for char in teams[0]:
+            if char == "@":
+                continue
+            if char == "(" and mode==0:
+                mode=1 #mean that we have collected our first datapoint 
+                awayTeam=datapoint.strip()
+                datapoint = ""
+                continue
+            if char == ")" and mode==1:
+                awayScore = datapoint
+                awayScore = int(awayScore) 
+                datapoint = ""
+                mode=0
+                continue
+            datapoint = datapoint+char
+        for char in teams[1]:
+            if char == "@":
+                 continue
+            if char == "(" and mode==0:
+                mode=1 #mean that we have collected our first datapoint 
+                homeTeam= datapoint.strip()
+                datapoint = ""
+                continue
+            if char == ")" and mode==1:
+                homeScore = datapoint
+                homeScore = int(homeScore) 
+                datapoint = ""
+                mode=0
+                continue
+            datapoint = datapoint+char
+        return(awayTeam, awayScore, homeTeam, homeScore)
+
+
+     
+
+#read file
 try:
     with open(file_path, 'r') as seasonData:
         #read the seasonData file line by line
@@ -15,8 +59,10 @@ try:
             print(parsedLine)
 except FileNotFoundError: # situation where the file is unable to be located
     print("File " + file_path + "could not be located")
-except Exception as e: # another 
+except Exception as e: # an error not related to file location has occured
     print("Unable to properly read file")
+
+parseIntoResults("New York Mets (0) @ Atlanta Braves (3)")
 
 
 
